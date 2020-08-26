c32661de52ac64f3ebb0d874714ac47cbaefba54
Teisha <colorfulspirit00@gmail.com>
2020-08-26 20:04:14 +0800

    components: header, menu, footer

---
+++ components/Header.vue
@@ -0,0 +1,19 @@
+<template>
+  <header id="header">
+    <div class="inner">
+
+      <!-- Logo -->
+        <a href="index.html" class="logo">
+          <span class="symbol"><img src="~assets/images/logo.svg" alt="" /></span><span class="title">Phantom</span>
+        </a>
+
+      <!-- Nav -->
+        <nav>
+          <ul>
+            <li><a href="#menu">Menu</a></li>
+          </ul>
+        </nav>
+
+    </div>
+  </header>
+</template>
